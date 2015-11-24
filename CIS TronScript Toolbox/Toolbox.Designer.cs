49db359fac116b4aeabeabddbdd8f9787b574f28
Benjamin Stout <stoutb@spu.edu>
2015-11-24 14:48:36 -0800

    init
    First push!
    - Integrated ConsoleControl
    - Implemented admin elevation
    - Added process queue
    - Changed title bar on process start

---
+++ CIS TronScript Toolbox/Toolbox.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace CIS_TronScript_Toolbox
+{
+    partial class Toolbox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.consoleControl = new ConsoleControl.ConsoleControl();
+            this.showConsoleButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // consoleControl
+            // 
+            this.consoleControl.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.consoleControl.IsInputEnabled = true;
+            this.consoleControl.Location = new System.Drawing.Point(12, 365);
+            this.consoleControl.Name = "consoleControl";
+            this.consoleControl.SendKeyboardCommandsToProcess = false;
+            this.consoleControl.ShowDiagnostics = false;
+            this.consoleControl.Size = new System.Drawing.Size(751, 0);
+            this.consoleControl.TabIndex = 1;
+            // 
+            // showConsoleButton
+            // 
+            this.showConsoleButton.Location = new System.Drawing.Point(258, 327);
+            this.showConsoleButton.Name = "showConsoleButton";
+            this.showConsoleButton.Size = new System.Drawing.Size(259, 23);
+            this.showConsoleButton.TabIndex = 0;
+            this.showConsoleButton.Text = "Show Console";
+            this.showConsoleButton.UseVisualStyleBackColor = true;
+            this.showConsoleButton.Click += new System.EventHandler(this.showConsoleButton_Click);
+            // 
+            // Toolbox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(775, 382);
+            this.Controls.Add(this.showConsoleButton);
+            this.Controls.Add(this.consoleControl);
+            this.MinimumSize = new System.Drawing.Size(791, 421);
+            this.Name = "Toolbox";
+            this.Text = "CIS TronScript Toolbox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ConsoleControl.ConsoleControl consoleControl;
+        private System.Windows.Forms.Button showConsoleButton;
+    }
+}
+
---
+++ CIS TronScript Toolbox/Toolbox.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS_TronScript_Toolbox
+{
+    public partial class Toolbox : Form
+    {
+        public class Process
+        {
+            public string File {get; set;}
+            public string Args {get; set;}
+            public Process()
+            {
+                File = "";
+                Args = "";
+            }
+        }
+        public Queue<Process> Processes = new Queue<Process>();
+
+        public Toolbox()
+        {
+            InitializeComponent();
+        }
+
+        private void newProcess(string file, string args)
+        {
+            //if process is already running or processes are queued
+            if (consoleControl.IsProcessRunning || Processes.Count > 0)
+            {
+                //create new process struct
+                Process qProcess = new Process();
+                qProcess.File = file;
+                qProcess.Args = args;
+                //push process into queue
+                Processes.Enqueue(qProcess);
+                //wait until current process finishes
+                while (consoleControl.IsProcessRunning) ;
+                //pull next process from queue
+                qProcess = Processes.Dequeue();
+                //start next process
+                consoleControl.StartProcess(qProcess.File, qProcess.Args);
+            }
+            else
+            {
+                //start new process
+                consoleControl.StartProcess(file, args);
+            }
+            //update form title
+            this.Text = "CIS Tronscript Toolbox - " + consoleControl.ProcessInterface.ProcessFileName;
+        }
+        private void showConsoleButton_Click(object sender, EventArgs e)
+        {
+            if(consoleControl.Height == 0)
+            {
+                showConsoleButton.Text = "Hide Console";
+                while(consoleControl.Height <= 180)
+                {
+                    consoleControl.Height++;
+                    Application.DoEvents();
+                }
+            }
+            else
+            {
+                showConsoleButton.Text = "Show Console";
+                while (consoleControl.Height > 0)
+                {
+                    consoleControl.Height--;
+                    Application.DoEvents();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (SystemInformation.BootMode != BootMode.FailSafeWithNetwork)
+            {
+                DialogResult dialogResult = MessageBox.Show("Boot into safe mode with networking?", "Reboot", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    newProcess("ipconfig", "/all");
+                    newProcess("bcdedit", " /set {current} safeboot network");
+                }
+            }
+        }
+    }
+}
