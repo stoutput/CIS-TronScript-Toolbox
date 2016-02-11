57b6c0cdf4fe9c75f5d553850dc8c0b801948836
Benjamin Stout <stoutb@spu.edu>
2016-02-10 16:01:05 -0800

    Concurrent processes
    Started implementing concurrent thread processing

--- CIS TronScript Toolbox/Toolbox.cs
+++ CIS TronScript Toolbox/Toolbox.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Concurrent;
 
 namespace CIS_TronScript_Toolbox
 {
@@ -22,39 +25,41 @@ namespace CIS_TronScript_Toolbox
                 Args = "";
             }
         }
-        public Queue<Process> Processes = new Queue<Process>();
+
+        //Thread-safe concurrent process collection
+        public BlockingCollection<Process> Processes = new BlockingCollection<Process>();
+        //Thread-safe background worker
+        private BackgroundWorker runProcess;
+
+        //Current user
+        public string User = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
 
         public Toolbox()
         {
             InitializeComponent();
         }
 
-        private void newProcess(string file, string args)
+        private void runProcess_DoWork(object sender, DoWorkEventArgs e)
         {
-            //if process is already running or processes are queued
-            if (consoleControl.IsProcessRunning || Processes.Count > 0)
-            {
-                //create new process struct
-                Process qProcess = new Process();
-                qProcess.File = file;
-                qProcess.Args = args;
-                //push process into queue
-                Processes.Enqueue(qProcess);
-                //wait until current process finishes
-                while (consoleControl.IsProcessRunning) ;
-                //pull next process from queue
-                qProcess = Processes.Dequeue();
-                //start next process
-                consoleControl.StartProcess(qProcess.File, qProcess.Args);
-            }
-            else
-            {
-                //start new process
-                consoleControl.StartProcess(file, args);
-            }
+            Process process = (Process) e.Argument;
+            //run console control process
+            consoleControl.StartProcess(process.File, process.Args);
+            //write process details to console
+            consoleControl.WriteOutput("Running " + process.File + process.Args + " as " + User + "...\n", Color.Green);
             //update form title
             this.Text = "CIS Tronscript Toolbox - " + consoleControl.ProcessInterface.ProcessFileName;
         }
+
+        private void newProcess(string file, string args)
+        {
+            //create new process struct
+            Process process = new Process();
+            process.File = file;
+            process.Args = args;
+            //queue process
+            Processes.Add(process);
+        }
+                
         private void showConsoleButton_Click(object sender, EventArgs e)
         {
             if(consoleControl.Height == 0)
@@ -79,14 +84,62 @@ namespace CIS_TronScript_Toolbox
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Enable console input
+            consoleControl.IsInputEnabled = true;
+
+            //Start script processing task
+            Task.Run(() =>
+            {
+                bool processing = false;
+
+                while (!Processes.IsCompleted)
+                {
+                    Process process = null;
+                    //Blocks if number.Count == 0
+                    try
+                    {
+                        process = Processes.Take();
+                    }
+                    catch (InvalidOperationException) { }
+
+                    if (process != null)
+                    {
+                        //start new process
+                        runProcess = new BackgroundWorker();
+                        runProcess.RunWorkerAsync(process);
+                        //set processing flag
+                        processing = true;
+                    }
+                    else if (!consoleControl.IsProcessRunning && processing)
+                    {
+                        //update form title
+                        this.Text = "CIS Tronscript Toolbox";
+                        //reset processing flag
+                        processing = false;
+                    }
+                }
+            });
+
             if (SystemInformation.BootMode != BootMode.FailSafeWithNetwork)
             {
                 DialogResult dialogResult = MessageBox.Show("Boot into safe mode with networking?", "Reboot", MessageBoxButtons.YesNo);
                 if (dialogResult == DialogResult.Yes)
                 {
                     newProcess("ipconfig", "/all");
-                    newProcess("bcdedit", " /set {current} safeboot network");
+                    newProcess("ping", "google.com");
+                    //Let process task consumer know we are done adding tasks
+                    //Ends processing task
+                    Processes.CompleteAdding();
+
+                    //windows 8+
+                    //newProcess("bcdedit", " /set {current} safeboot network");
+                    //windows 7-
+                    //newProcess("bcdedit", " /set {default} safeboot network");
                 }
+            }
+            else
+            {
+                //bcdedit /deletevalue {default} safeboot (network???) 
             }
         }
     }
